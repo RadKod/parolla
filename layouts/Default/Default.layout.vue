84806d820235be15a4d065010f279131f09e2e71
selimdoyranli <selim@radkod.com>
2023-03-26 17:32:21 +0300

    :money_with_wings: insert ads

--- layouts/Default/Default.layout.vue
+++ layouts/Default/Default.layout.vue
@@ -3,6 +3,10 @@
   // Preloader
   AppPreloader
 
+  .layout__ad.layout__ad--left
+    // image_vertical_flex_left
+    AppAd(:data-ad-slot="2717771834")
+
   // Inner
   .layout__inner
     // App Header
@@ -12,17 +16,23 @@
     main.layout__main
       // Router View
       nuxt.router-view
+
+  .layout__ad.layout__ad--right
+    // image_vertical_flex_right
+    AppAd(:data-ad-slot="8500032812")
 </template>
 
 <script>
 import { defineComponent } from '@nuxtjs/composition-api'
 import { AppPreloader } from '@/components/Preloader'
 import { AppHeader } from '@/components/Header'
+import { AppAd } from '@/components/Ad'
 
 export default defineComponent({
   components: {
     AppPreloader,
-    AppHeader
+    AppHeader,
+    AppAd
   },
   setup() {}
 })
